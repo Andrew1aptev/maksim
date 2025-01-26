feaba3f760bad3da57c86440b13fedc7a9962156
Andrew Laptev <laptewandrew@gmail.com>
2025-01-26 18:28:25 +0300

    add burger menu

--- js/main.js
+++ js/main.js
@@ -1,10 +1,11 @@
 const header = document.querySelector("header");
+const headerButton = document.querySelector(".header__button")
 function headerScroll () {
 const coords = window.pageYOffset; // получаем число пикселей на которое пролистали документ в данный момент
     if(coords === 0){
-    header.classList.remove("header-scroll") //удаляем класс header-scroll
+    header.classList.remove("header--scroll") //удаляем класс header-scroll
     }else   {
-     header.classList.add("header-scroll") //добавляем класс header-scroll
+     header.classList.add("header--scroll") //добавляем класс header-scroll
     }
 console.log(coords); // выводим координаты в консоль
     }
@@ -12,3 +13,9 @@ console.log(coords); // выводим координаты в консоль
     window.addEventListener("scroll", headerScroll)
 
 
+function addClassOnClick () {
+  headerButton.addEventListener("click", () =>{
+    header.classList.toggle("open")
+  })
+}
+addClassOnClick()
